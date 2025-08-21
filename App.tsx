aff1fe8b47373339dd41d795094370e24552fdfd
Akaki <31448057+khotcholava@users.noreply.github.com>
2025-08-21 12:10:35 +0400

    Implement ws connection

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {useEffect, useRef} from "react";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  TapGestureHandler,
+} from "react-native-gesture-handler";
+
+const AIRTRACK_WS_URL = "ws://192.168.0.101:9002";
 
 export default function App() {
+  const ws = useRef<WebSocket | null>(null)
+
+  useEffect(() => {
+    ws.current = new WebSocket(AIRTRACK_WS_URL);
+
+    ws.current.onopen = () => {
+      console.log("Connected to AirTrack");
+    };
+
+    ws.current.onmessage = (msg) => {
+      console.log(`Date received: ${msg.data}`);
+    }
+
+    return () => {
+      ws.current?.close();
+    }
+  })
+
+
+  const handlePan = (event: any) => {
+    const { translationX, translationY } = event.nativeEvent;
+    if (ws.current?.readyState === WebSocket.OPEN) {
+      ws.current.send(
+        JSON.stringify({
+          type: "move",
+          dx: Math.round(translationX),
+          dy: Math.round(translationY),
+        })
+      );
+    }
+  };
+
+  const handleSingleTap = () => {
+    if(ws.current?.readyState === WebSocket.OPEN) {
+      ws.current.send(
+        JSON.stringify({
+          type: "click",
+          button: "left"
+        })
+      )
+    }
+  }
+  const handleDoubleTap = () => {
+    if(ws.current?.readyState === WebSocket.OPEN) {
+      console.log("Double tap");
+      ws.current.send(
+        JSON.stringify({
+          type: "click",
+          button: "right"
+        })
+      )
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <TapGestureHandler
+        onActivated={handleSingleTap}
+        numberOfTaps={1}
+        waitFor="doubleTap"
+      >
+        <TapGestureHandler id="doubleTap" onActivated={handleDoubleTap} numberOfTaps={2}>
+          <PanGestureHandler onGestureEvent={handlePan}>
+            <View style={styles.trackpad} />
+          </PanGestureHandler>
+        </TapGestureHandler>
+      </TapGestureHandler>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  trackpad: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#111",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
